6b74a68677961fef1342a76a907d9f248a2ba440
Martin Mojto <xmojtom1@stuba.sk>
2018-11-30 16:52:18 +0100

    my3Dfun1B

---
+++ my3Dfun1.m
@@ -0,0 +1,21 @@
+%% my3Dfun1
+% Function domain
+[x,y] = meshgrid(-4:0.01:4,-2:0.01:2);
+% Function values
+z = x.*exp(-x.^2-y.^2);
+% Figure initialization
+figure(1)
+% Plotting figure
+surfc(x,y,z);
+% Title
+title('z_B = x exp(-x^2-y^2)');
+% Labels
+xlabel('x_B');
+ylabel('y_B');
+zlabel('z_B');
+% Grid
+grid minor;
+% Colormap
+colormap(cool);
+% Shading
+shading flat
